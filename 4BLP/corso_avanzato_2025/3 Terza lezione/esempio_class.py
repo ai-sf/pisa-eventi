4e5cbcea313a7596146746052b4c58aa079e6621
Francesco <40426275+Fr4nci@users.noreply.github.com>
2025-03-13 15:55:54 +0100

    terza lezione avanzata

---
+++ 4BLP/corso_avanzato_2025/3 Terza lezione/esempio_class.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+Anna = Persona("Anna", "Becheroni")
+Matteo = Persona("Matteo", "Cunsolo")
+print(Anna.name)
+print(Anna.a)
+print(Anna.surname)
+print(Matteo.a)
